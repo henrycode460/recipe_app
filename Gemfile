a9e5a4c53ef8caa0fbe9be4f5c4e2ca9cdd50eb6
Muhammad Shaheer <shaheer.chry@gmail.com>
2022-10-29 13:57:41 +0500

    Add Authentication

--- Gemfile
+++ Gemfile
@@ -45,6 +45,7 @@ gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', require: false
+gem 'ffi'
 
 # Use Sass to process CSS
 # gem "sassc-rails"
@@ -56,6 +57,7 @@ group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem 'database_cleaner'
   gem 'debug', platforms: %i[mri mingw x64_mingw]
+  gem 'rails-controller-testing'
   gem 'rspec-rails'
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,15 +5,14 @@ Rails.application.routes.draw do
     get 'users/sign_out' => 'devise/sessions#destroy'
   end
 
-  root 'users#new'
+  root 'foods#index'
    
-  resources :recipe_foods
-  resources :foods
-  get '/users', to: 'users#new'
+  resources :foods #[:index, :destroy, :new, :show]
+  get '/users', to: 'foods#index'
 
-  resources :users do
-    resources :recipes, only: [:index, :destroy, :new, :show]
-  end
+  resources :users 
+  resources :recipe_foods #[:index, :destroy, :new, :show]
+  resources :recipes, only: [:index, :destroy, :new, :create, :show]
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
