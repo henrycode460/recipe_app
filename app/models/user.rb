719e52f580999c48756fd847b4339939219131bc
Henry <dokinruss@gmail.com>
2022-10-29 13:35:32 +0000

    FIX LINTER ERROR

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-    has_many :foods, foreign_key: :user_id
-    validates :name, presence: true
+  has_many :foods, foreign_key: :user_id
+  validates :name, presence: true
 
-    has_many :foods, dependent: :destroy
+  has_many :foods, dependent: :destroy
 end
