57f019ed963d806fdf115442817664be42781cc2
Muhammad Shaheer <shaheer.chry@gmail.com>
2022-10-29 11:41:16 +0500

    Setup device authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,22 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  devise_scope  :user do
+    get 'users/sign_out' => 'devise/sessions#destroy'
+  end
+
+  root 'users#new'
+   
   resources :recipe_foods
   resources :foods
   get '/users', to: 'users#new'
 
-  resources :recipes, only: [:index, :destroy, :new, :show]
-  resources :users
+  resources :users do
+    resources :recipes, only: [:index, :destroy, :new, :show]
+  end
+  
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
 end
