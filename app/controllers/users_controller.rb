b4c9bbc5b64d1b1b020b6157dd877eaa11f21722
Muhammad Shaheer <shaheer.chry@gmail.com>
2022-10-29 11:41:26 +0500

    Setup device authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[show edit update destroy]
+  before_action :authenticate_user!
 
   # GET /users/1 or /users/1.json
-  def show; end
+  # def show; end
 
   # GET /users/new
   def new
--- db/schema.rb
+++ db/schema.rb
@@ -25,6 +25,22 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_26_105932) do
     t.index ["user_id"], name: "index_foods_on_user_id"
   end
 
+  create_table "models", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_models_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
+  end
+
   create_table "recipe_foods", force: :cascade do |t|
     t.integer "quantity"
     t.bigint "recipe_id", null: false
@@ -51,6 +67,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_26_105932) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "foods", "users"
